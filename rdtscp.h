700b14edf8705c81d9b112301c47eef99d5be2c8
Owen Flaherty <thatsimplekid@gmail.com>
2018-01-26 08:58:48 +0000

    Initial Commit

---
+++ rdtscp.h
@@ -0,0 +1,22 @@
+/* rdtscp.h - taken from Fastly's uslab                     
+   https://github.com/fastly/uslab/blob/master/rdtscp.h */
+
+#ifndef _RDTSCP_H_
+#define _RDTSCP_H_
+
+#include <stdint.h>
+
+static inline uint64_t
+rdtscp(void)
+{
+	uint32_t eax, edx;
+
+	__asm__ __volatile__("rdtscp"
+		: "=a" (eax), "=d" (edx)
+		:
+		: "%ecx", "memory");
+
+	return (((uint64_t)edx << 32) | eax);
+}
+
+#endif
